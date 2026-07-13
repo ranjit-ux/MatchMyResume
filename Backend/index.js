141d094f6c693601e1818c72669579f73a6ecc4e
Ranjit Kumar Singh <ranjitksingh.079@gmail.com>
2026-07-13 19:40:34 +0530

    Allow Vercel frontend

--- Backend/index.js
+++ Backend/index.js
@@ -18,7 +18,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://matchmyresume-frontend.onrender.com"
+      "https://match-my-resume-sv1j.vercel.app"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
